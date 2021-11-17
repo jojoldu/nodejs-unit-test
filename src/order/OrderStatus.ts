9f2c8e88cea70c31cda2d012af38bc486275d5f2
jojoldu <jojoldu@gmail.com>
2021-11-17 10:07:32 +0900

    TimeUnit 개발 시작

--- src/order/OrderStatus.ts
+++ src/order/OrderStatus.ts
@@ -2,3 +2,4 @@ export enum OrderStatus {
     APPROVAL = 'APPROVAL',
     CANCEL = 'CANCEL',
 }
+
---
+++ test/TimeUnit.spec.ts
@@ -0,0 +1,6 @@
+
+describe('TimeUnit', () => {
+    it('1초는 1000 millis로 치환된다', () => {
+        const a = TimeUnit;
+    });
+});
