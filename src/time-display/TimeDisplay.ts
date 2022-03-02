990425bd019f101bd4b7f25b7158e020297484d0
jojoldu <jojoldu@gmail.com>
2022-03-02 09:08:55 +0900

    조건부 로직 2번째 예제 추가

---
+++ src/time-display/TimeDisplay.ts
@@ -0,0 +1,17 @@
+import { DateTimeFormatter, LocalDateTime } from 'js-joda';
+
+export class TimeDisplay {
+    display (time: LocalDateTime): string {
+        if(time.hour() === 0 && time.minute() === 0) {
+            return 'Midnight'
+        }
+
+        if (time.hour() === 12 && time.minute() === 0) {
+            return 'Noon';
+        }
+
+        return time.format(DateTimeFormatter.ofPattern(
+            'HH:mm:ss',
+        ))
+    }
+}
--- test/conditional-assert/ConditionalAssert.spec.ts
+++ test/conditional-assert/ConditionalAssert.spec.ts
@@ -1,6 +1,27 @@
 import { Calculator } from '../../src/calculator/Calculator';
+import { DateTimeFormatter, LocalDateTime } from 'js-joda';
+import { TimeDisplay } from '../../src/time-display/TimeDisplay';
 
 describe('조건부 검증 피하기', () => {
+    it('가변결과 검증하는 경우', () => {
+        const now = LocalDateTime.now();
+        const sut = new TimeDisplay();
+        const result = sut.display(now);
+
+        let actual;
+        if(now.hour() === 0 && now.minute() === 0) {
+            actual = 'Midnight';
+        } else if (now.hour() === 12 && now.minute() === 0) {
+            actual = 'Noon';
+        } else {
+            actual = now.format(DateTimeFormatter.ofPattern(
+                'HH:mm:ss',
+            ));
+        }
+
+        expect(result).toBe(actual);
+    });
+
     it('프로덕션 로직을 테스트에서 사용하는 경우', () => {
         const sut = new Calculator();
         let result;
