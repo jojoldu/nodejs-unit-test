2ff5e54f15eee50a8d3f857ba1ab65e2efed32db
jojoldu <jojoldu@gmail.com>
2022-03-04 09:46:49 +0900

    Good 테스트 케이스 추가

--- test/conditional-assert/ConditionalAssert.spec.ts
+++ test/conditional-assert/ConditionalAssert.spec.ts
@@ -43,15 +43,12 @@ describe('조건부 검증 피하기', () => {
       });
     });
 
-
-
-
-    it('프로덕션 로직을 테스트에서 사용하는 경우', () => {
+    it('[Bad] 프로덕션 로직을 테스트에서 사용하는 경우', () => {
         const sut = new Calculator();
         let result;
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
-                const actual = sut.calculate( i, j );
+                const actual = sut.calculate(i, j);
 
                 if (i==3 && j==4)  // 특이케이스
                     result = 8;
@@ -63,12 +60,34 @@ describe('조건부 검증 피하기', () => {
         }
     });
 
-    describe('주문 상태 검증', () => {
-        it('추출된 중복 테스트 코드#1', () => {
+    describe('[Good] 프로덕션 로직을 테스트에서 사용하는 경우', () => {
+        it('[Good] Calculator 에 3, 4가 입력되면 8이 반환된다', () => {
+            const sut = new Calculator();
+
+            const result = sut.calculate(3, 4);
+
+            expect(result).toBe(8);
+        });
+
+        it.each([
+            [3, 4, 8],
+            [1, 1, 2],
+        ])("num1=%s, num2=%s 이면 actual=%s", (num1, num2, actual) => {
+            const sut = new Calculator();
+
+            const result = sut.calculate(num1, num2);
+
+            expect(result).toBe(actual);
+        });
+    });
+
+
+    describe('중복 코드 추출로 인한 다중 검증', () => {
+        it('[Bad] 추출된 중복 테스트 코드#1', () => {
             testOrder(OrderStatus.COMPLETED);
         });
 
-        it('추출된 중복 테스트 코드#2', () => {
+        it('[Bad] 추출된 중복 테스트 코드#2', () => {
             testOrder(OrderStatus.CANCEL);
         });
 
@@ -77,11 +96,30 @@ describe('조건부 검증 피하기', () => {
             order.status = status;
 
             if(status === OrderStatus.COMPLETED) {
-                expect(order.isCompleted()).toBeTruthy();
+                expect(order.isCompleted()).toBe(true); // boolean 타입까지 일치하는 것을 확인하기 위해
             } else if (status === OrderStatus.CANCEL) {
-                expect(order.isCanceled()).toBeTruthy();
+                expect(order.isCanceled()).toBe(true);
             }
         }
+
+        it('[Good] 추출된 중복 테스트 코드#1', () => {
+            const sut = createOrder(OrderStatus.COMPLETED);
+
+            expect(sut.isCompleted()).toBe(true);
+        });
+
+        it('[Good] 추출된 중복 테스트 코드#2', () => {
+            const sut = createOrder(OrderStatus.CANCEL);
+
+            expect(sut.isCanceled()).toBe(true);
+        });
+
+        function createOrder(status: OrderStatus): Order {
+            const order = new Order();
+            order.status = status;
+            return order;
+        }
+
     });
 
 });
