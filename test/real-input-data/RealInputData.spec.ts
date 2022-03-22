367e318f48064dbee6c28238e1d9db49f57ee9c9
jojoldu <jojoldu@gmail.com>
2022-03-22 21:52:48 +0900

    샘플 예제 추가

---
+++ test/real-input-data/RealInputData.spec.ts
@@ -0,0 +1,23 @@
+describe('현실적인 Given 데이터 사용하기', () => {
+  it('[거짓성공] 규칙에 맞는 이름을 등록하면 성공한다', () => {
+    const addProductResult = addProduct("test");
+    expect(addProductResult).toBe(true);
+  });
+
+  it('[실패] 규칙에 맞는 이름을 등록하면 성공한다', () => {
+    const addProductResult = addProduct("te st");
+    expect(addProductResult).toBe(true);
+  });
+
+});
+
+function addProduct(name:string) {
+  // 공백을 허용하지 않은 정규식
+  const productNameRegexNoSpace = /^\S*$/;
+
+  if(!productNameRegexNoSpace.test(name)) {
+    return false;
+  }
+
+  return true;
+}
