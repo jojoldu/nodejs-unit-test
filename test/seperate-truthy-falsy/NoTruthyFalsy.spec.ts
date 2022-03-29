65de84e5ef0a00ded59ca381da1b258e427b32c5
jojoldu <jojoldu@gmail.com>
2022-03-29 21:18:42 +0900

    false 테스트 추가

---
+++ test/seperate-truthy-falsy/NoTruthyFalsy.spec.ts
@@ -0,0 +1,28 @@
+describe('toBe(false)와 toBeFalsy() 구분하기', () => {
+  it('falsy로 검증하면 0도 통과된다', () => {
+    const result = calculate();
+
+    expect(result).toBeFalsy();
+  });
+
+  it('toBe(false)로 검증하면 0은 통과되지 못한다', () => {
+    const result = calculate();
+
+    expect(result).toBe(false);
+
+  });
+
+
+});
+
+function calculate() {
+  return 0;
+}
+
+function requestEmpty() {
+  return '';
+}
+
+function requestFalse() {
+  return false;
+}
