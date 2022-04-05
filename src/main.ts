3c72284c63787ac6d301a6d2159c08064c13be3a
jojoldu <jojoldu@gmail.com>
2022-04-05 19:40:49 +0900

    PR에서의 소나큐브 적용 테스트1

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('PR을 위한 테스트');
 }
 bootstrap();
