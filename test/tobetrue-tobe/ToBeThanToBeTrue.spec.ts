fba47fdb63a00c82cccc6dd3caba5d1edcac341b
jojoldu <jojoldu@gmail.com>
2022-04-10 01:16:50 +0900

    완성

--- test/tobetrue-tobe/ToBeThanToBeTrue.spec.ts
+++ test/tobetrue-tobe/ToBeThanToBeTrue.spec.ts
@@ -1,13 +1,27 @@
-
 describe('toBe(true)와 toBe(b)', () => {
+  describe('등호', () => {
+    it('getCount의 결과는 5보다 크다', () => {
+      const result = getCount();
+
+      expect(result > 5).toBe(true);
+    });
+
+    it('getCount의 결과는 5보다 크다', () => {
+      const result = getCount();
+
+      expect(result).toBeGreaterThan(5);
+    });
+
+  });
+
   describe('테스트 성공', () => {
-    it('toBe(true)', () => {
+    it('getCount의 값은 10과 동일하다', () => {
       const count = getCount();
 
       expect(count === 10).toBe(true);
     });
 
-    it('toBe(b)', () => {
+    it('getCount의 값은 10이다', () => {
       const count = getCount();
 
       expect(count).toBe(10);
