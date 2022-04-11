d7c3a86b9692f0f611b27efbf45f42f41fe72dfc
jojoldu <jojoldu@gmail.com>
2022-04-11 21:20:00 +0900

    faker 예제 코드 작업중

--- test/real-input-data/RealInputData.spec.ts
+++ test/real-input-data/RealInputData.spec.ts
@@ -1,3 +1,5 @@
+import faker from '@faker-js/faker';
+
 describe('현실적인 Given 데이터 사용하기', () => {
   it('[거짓성공] 규칙에 맞는 이름을 등록하면 성공한다', () => {
     const addProductResult = addProduct("test");
@@ -9,9 +11,14 @@ describe('현실적인 Given 데이터 사용하기', () => {
     expect(addProductResult).toBe(true);
   });
 
+  it.skip('[실패] 유효한 값이 추가되면 성공한다', () => {
+    const addProductResult = addProduct(faker.commerce.productName(), faker.random.number());
+    //생성된 무작위 인풋: {'Sleek Cotton Computer',  85481}
+    expect(addProductResult).toBe(true);
+  });
 });
 
-function addProduct(name:string) {
+function addProduct(name:string, number?: number) {
   // 공백을 허용하지 않은 정규식
   const productNameRegexNoSpace = /^\S*$/;
 
