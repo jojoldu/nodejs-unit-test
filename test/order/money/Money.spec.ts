96a5359986896130c76d2e2d731c46ee480543a3
jojoldu <jojoldu@gmail.com>
2022-10-01 11:06:04 +0900

    Money 테스트 추가

--- test/order/money/Money.spec.ts
+++ test/order/money/Money.spec.ts
@@ -1,6 +1,14 @@
 import { Money } from '../../../src/order/money/Money';
 
 describe('Money', () => {
+  it('음수가 들어오면 에러가 발생한다', () => {
+    const amount = -1;
+
+    expect(() => {
+      new Money(amount);
+    }).toThrowError(new Error('금액은 -가 될 수 없습니다. amount=-1'));
+  });
+
   it('소수점이 들어오면 에러가 발생한다', () => {
     const amount = 0.1;
 
