ab9ff7a754338a40a785203d25a0f1034ad731a3
jojoldu <jojoldu@gmail.com>
2023-04-16 11:03:28 +0900

    완성

--- src/logger/1/tobe/validateSignIn.ts
+++ src/logger/1/tobe/validateSignIn.ts
@@ -11,5 +11,6 @@ export async function validateSignIn(id: string, password: string) {
 
   if(matchResult.failCount > 5) {
     log.error(`id=${id} 의 로그인 실패 횟수가 5회를 초과했습니다.`);
+    throw new Error('로그인 실패 횟수 초과로 30분간 로그인을 시도할 수 없습니다.');
   }
 }
