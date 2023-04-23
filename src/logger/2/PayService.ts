3d0cf9ec4cadb4d63ae87da5658bc1309ee9381e
jojoldu <jojoldu@gmail.com>
2023-04-23 23:15:48 +0900

    ERROR 캡쳐를 위한 await 추가

--- src/logger/2/PayService.ts
+++ src/logger/2/PayService.ts
@@ -5,18 +5,17 @@ import { Injectable } from '@nestjs/common';
 export class PayService {
   async cancel(orderNo) {
     try {
-      return payRepository.cancelOrder(orderNo);
+      return await payRepository.cancelOrder(orderNo);
     } catch (e) {
       log.error(`주문취소 실패, orderNo=${orderNo}`, e);
-      throw new Error(`주문취소 실패, orderNo=${orderNo}`);
+      throw new Error(`해당하는 주문이 없습니다, orderNo=${orderNo}`);
     }
   }
 
   async cancel2(orderNo) {
     try {
-      return payRepository.cancelOrder(orderNo);
+      return await payRepository.cancelOrder(orderNo);
     } catch (e) {
-      log.error(`주문취소 실패, orderNo=${orderNo}`, e);
       throw new Error(`주문취소 실패, orderNo=${orderNo}`);
     }
   }
