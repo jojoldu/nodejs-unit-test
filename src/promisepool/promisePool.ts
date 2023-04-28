e0d3206201eb052a417b3d4a6c2431e6e8e22daa
jojoldu <jojoldu@gmail.com>
2023-04-28 23:57:46 +0900

    0.5 ~ 3초 사이 랜덤하게 sleep

--- src/promisepool/promisePool.ts
+++ src/promisepool/promisePool.ts
@@ -1,5 +1,9 @@
 import { sleep } from '../throw/sleep';
 
 export function run() {
-  const promises: Promise<void>[] = Array.from({ length: 1000 }, (_, i) => sleep(i));
+  const promises: Promise<void>[] = Array.from({ length: 1000 }, () => sleep(getRandomTime(500, 3000)));
+}
+
+function getRandomTime(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
