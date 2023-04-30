b5b4d88372c070155b4ac5f7a10aae5a8dca0f12
jojoldu <jojoldu@gmail.com>
2023-04-30 23:16:35 +0900

    예제 교정

--- src/promisepool/promisePool.ts
+++ src/promisepool/promisePool.ts
@@ -1,15 +1,16 @@
-import { sleep } from '../throw/sleep';
+import { clear, count, insert, sleep } from '../throw/sleep';
 import _ from 'lodash';
 import { PromisePool } from '@supercharge/promise-pool';
 
 export async function measure(chunkSize = 100) {
-  const times = Array.from({length: 1000}, () => getRandomTime(300, 2000));
+  const times = Array.from({length: 1000}, () => getRandomTime(500, 2000));
   await measurePromiseAll(times, chunkSize);
   await measurePromisePool(times, chunkSize);
 }
 
 async function measurePromiseAll(times: number[], chunkSize: number) {
-  const promises = times.map(time => sleep(time));
+  clear();
+  const promises = times.map(time => insert(time));
   const chunkAll = _.chunk(promises, chunkSize);
   const startTime = performance.now();
 
@@ -20,21 +21,22 @@ async function measurePromiseAll(times: number[], chunkSize: number) {
   const endTime = performance.now();
   const elapsedTime = endTime - startTime;
 
-  console.log(`PromiseAll Total time elapsed: ${elapsedTime} ms`);
+  console.log(`PromiseAll Total time elapsed: ${elapsedTime} ms, count: ${count()})`);
 }
 
 async function measurePromisePool(times: number[], chunkSize: number) {
+  clear();
   const startTime = performance.now();
 
   const { results, errors } = await PromisePool
     .for(times)
     .withConcurrency(chunkSize)
-    .process(sleep);
+    .process(insert);
 
   const endTime = performance.now();
   const elapsedTime = endTime - startTime;
 
-  console.log(`PromisePool Total time elapsed: ${elapsedTime} ms`);
+  console.log(`PromisePool Total time elapsed: ${elapsedTime} ms, count: ${count()})`);
 }
 
 function getRandomTime(min: number, max: number): number {
--- src/throw/sleep.ts
+++ src/throw/sleep.ts
@@ -8,3 +8,19 @@ export async function sleepAndReturnTime(ms: number) {
   await sleep(ms);
   return ms;
 }
+
+export let mockDatabase = [];
+
+export function clear() {
+  mockDatabase = [];
+}
+
+export function count() {
+  return mockDatabase.length;
+}
+
+export async function insert(ms: number) {
+  await sleep(ms);
+  mockDatabase.push(ms);
+  return ms;
+}
