40c276a015e49407a1434759476114de3828cad8
jojoldu <jojoldu@gmail.com>
2023-05-31 18:58:40 +0900

    private 분기 추가

--- src/user/UserRepository.ts
+++ src/user/UserRepository.ts
@@ -1,4 +1,3 @@
-import { Order } from '../order/Order';
 import { User } from './User';
 
 export class UserRepository {
@@ -10,5 +9,17 @@ export class UserRepository {
 
   async findById(id: number): Promise<User> {
     return UserRepository.database.get(id);
+  }
+  async findActiveUser(name: string) {
+    return this.findByIdAndIsDeletedAndIsBlocked(name, false, false);
+  }
+
+  async findBlockedUser(name: string) {
+    return this.findByIdAndIsDeletedAndIsBlocked(name, false);
+  }
+
+  async findByIdAndIsDeletedAndIsBlocked(name: string, isBlocked?: boolean, isDeleted?: boolean) {
+    console.log(`find user: name=${name}, isBlocked=${isBlocked}, isDeleted=${isDeleted})`);
+    return `select * from user where name = ${name} and isBlocked = ${isBlocked} and isDeleted = ${isDeleted}`;
   }
 }
